b3e33c98e75c095940e4d83c315a769c8a4c1b60
Sebastian Nicolas Picardi <36540321+sebastiap@users.noreply.github.com>
2022-05-04 22:37:29 -0300

    Modal with Verb Tenses

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -1,18 +1,31 @@
 import { useState, useEffect } from "react";
 import { colorPallete } from "../resources/colorPallete";
+import text from "../resources/pageText";
 
 function Modal() {
   const [miscPhrase, setMiscPhrase] = useState(" TV");
   const [verbPhrase, setVerbPhrase] = useState("watch");
+  var randomColor = "grey";
+  var antColor = "whitewheat";
+  function getNewColor() {
+    randomColor = colorPallete[Math.floor(Math.random() * colorPallete.length)];
+    if (randomColor === antColor) {
+      return "whitewheat";
+    } else {
+      return randomColor;
+    }
+  }
   return (
     <div>
       <div class="modal-content">
         <span class="close">&times;</span>
-        <p>Simple Past</p>
-        <p>Past Continous</p>
-        <p>Past Perfect </p>
-        <p>Past Perfect Continous</p>
-        <p>Simple Present</p>
+        <div className=".modal-content-container">
+          {text.map(([linea, linea2]) => (
+            <p class="modal-content-text" style={{ color: getNewColor() }}>
+              <b>{linea}</b> - {linea2}
+            </p>
+          ))}
+        </div>
       </div>
     </div>
   );
---
+++ src/resources/pageText.jsx
@@ -0,0 +1,43 @@
+const text = [
+  ["Simple past", " for actions starting and ending in the past."],
+  [
+    "Past continuous",
+    "for actions starting in the past and continuing to the present."
+  ],
+  [
+    "Past perfect",
+    "for actions that started and ended in the past before another action that is also in the past."
+  ],
+  [
+    "Past perfect continuous",
+    "for actions that were going on in the past up until another action in the past happened."
+  ],
+  ["Simple present", "to express habits and general truths."],
+  ["Present continuous", "to talk about an action happening now."],
+  [
+    "Present perfect",
+    "for repeated actions, actions where the time is not important, and actions that began in the past but are not finished yet."
+  ],
+  [
+    "Present perfect continuous",
+    "to talk about ongoing actions where both the process and the result are important."
+  ],
+  [
+    "Simple future",
+    "refers to a time later than now, and expresses facts or certainty. In this case there is no 'attitude'."
+  ],
+  [
+    "Future continuous",
+    "refers to an unfinished action or event that will be in progress at a time later than now. The future continuous is used for quite a few different purposes.The future continuous can be used for predicting or guessing about future events.In the interrogative form, the future continuous can be used to ask politely for information about the future."
+  ],
+  [
+    "Future perfect",
+    "tense refers to a completed action in the future. When we use this tense we are projecting ourselves forward into the future and looking back at an action that will be completed some time later than now. It is most often used with a time expression."
+  ],
+  [
+    "future perfect Continous",
+    "Like the future perfect simple, this form is used to project ourselves forward in time and to look back. It refers to events or actions that are currently unfinished but will be finished at some future time. It is most often used with a time expression."
+  ]
+];
+
+export default text;
